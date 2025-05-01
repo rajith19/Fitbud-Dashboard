9033398d7bf309843f9e624234f0f5d7307c79e6
rajith27 <rajithvgopalm@gmail.com>
2025-04-30 20:51:48 -0400

    fix

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,32 @@
-import { createMiddlewareClient } from "@supabase/ssr";
+import { createServerClient } from "@supabase/ssr";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import type { Database } from "@/types/supabase";
 
 export async function middleware(req: NextRequest) {
-  const supabase = createMiddlewareClient<Database>({ req });
+  const supabase = createServerClient<Database>(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get: (key) => req.cookies.get(key)?.value,
+        set: (key, value, options) => {
+          req.cookies.set({
+            name: key,
+            value,
+            ...options,
+          });
+        },
+        remove: (key, options) => {
+          req.cookies.set({
+            name: key,
+            value: "",
+            ...options,
+          });
+        },
+      },
+    }
+  );
 
   const {
     data: { session },
@@ -12,7 +34,6 @@ export async function middleware(req: NextRequest) {
 
   const url = req.nextUrl.clone();
   const pathname = url.pathname;
-
   const isAuthPage = pathname.startsWith("/signin") || pathname.startsWith("/signup");
 
   if (!session && !isAuthPage) {
